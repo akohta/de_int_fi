66ffed0e2ddc7623b2100585ee40502fac2c889d
akohta <gml.yj21364@gmail.com>
2022-05-19 00:13:09 +0900

    add comments

--- example.c
+++ example.c
@@ -31,7 +31,7 @@ int main()
   int erc;
 
   pa.omega=omega;
-  // Example 1 ( sin type function )
+  printf("-- Example 1 ( sin type function ) --\n");
   printf("I    = int_0^inf sin(x)/x dx\n");
   printf("     = %15.14e ( = pi/2 )\n",M_PI/2.0);
   pa.cc=0;
@@ -41,7 +41,7 @@ int main()
   Id=deintd_ffs(s2,&pa,a,omega,eps,&erc);
   printf("I_ffs= %15.14e, erc = %d, function call %d\n\n",Id,erc,pa.cc);
 
-  // Example 2 ( cos type function )
+  printf("-- Example 2 ( cos type function ) --\n");
   printf("I    = int_0^inf cos(x)/(x^2+1) dx\n");
   printf("     = %15.14e ( = pi/(2*e) )\n",M_PI/(2.0*exp(1))); 
   pa.cc=0;
@@ -51,7 +51,7 @@ int main()
   Id=deintd_ffc(c2,&pa,a,omega,eps,&erc);
   printf("I_ffc= %15.14e, erc = %d, function call %d\n\n",Id,erc,pa.cc);
 
-  // Example 3
+  printf("-- Example 3 --\n");
   printf("I    = int_0^inf sin(x)/(x+1+i) dx\n");
   printf("     = Ci(z)*sin(z)+(pi/2-Si(z))*cos(z), z=1+i. Ci(z) : cosine integral function, Si(z) : sine integral function\n");
   printf("     = 4.79410254818214e-01 -2.63993546979857e-01 i \n");
@@ -59,9 +59,9 @@ int main()
   Iz=deintz_ffs(zs,&pa,a,1.0,eps,&erc);
   printf("I_ffs= %15.14e %+15.14e i, erc = %d, function call %d\n\n",creal(Iz),cimag(Iz),erc,pa.cc);
 
-  // Example 4
+  printf("-- Example 4 --\n");
   printf("I    = 2/(pi*i) * int_0^inf exp(-(x-i)/sqrt(2))*cos(sqrt(x^2-2*i*x)/sqrt(2))/sqrt(x^2-2*i*x) dx\n");
-  printf("     = H_0^(1)(1), H_0^(1)(x) : hankel function 1st-kind order 1\n");
+  printf("     = H_0^(1)(1), H_0^(1)(x) : hankel function 1st-kind order 0\n");
   printf("     = 7.65197686557966e-01 +8.82569642156769e-02 i\n");
   pa.cc=0;
   Iz=2.0/(M_PI*I)*deintz_ffc(zc,&pa,a,1.0/sqrt(2.0),eps,&erc);
